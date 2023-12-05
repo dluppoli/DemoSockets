63c76d40cd6747c3fd8643d0e7641d840badecd6
Davide Luppoli <davide@davideluppoli.it>
2023-12-05 11:55:34 +0100

    UdpClient

---
+++ ClientUdp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientUdp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Premi un tasto per iniare...");
+            Console.ReadLine();
+
+            //Creare Socket (ip + porta)
+            //var endpoint = new IPEndPoint(IPAddress.Loopback, 8888);
+            UdpClient socket = new UdpClient();
+
+            //Collegarsi al server (ip + porta)
+            socket.Connect(IPAddress.Loopback, 8888);
+
+            //Invio dei dati
+            Console.WriteLine("Inserisci il messaggio da inviare:");
+            string message = Console.ReadLine();
+            byte[] messageBytes = System.Text.Encoding.ASCII.GetBytes(message);
+            await socket.SendAsync(messageBytes, messageBytes.Length);
+
+            //Ricezione della risposta
+            //byte[] responseBytes = new byte[10000];
+            var receiveResult = await socket.ReceiveAsync();
+            byte[] responseBytes = receiveResult.Buffer;
+            string response = System.Text.Encoding.ASCII.GetString(responseBytes).TrimEnd('\0');
+            Console.WriteLine(response);
+            Console.ReadLine();
+
+            //Chiusura Socket
+            socket.Dispose();
+        }
+    }
+}
---
+++ ServerUdp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerUdp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Creazione della socket
+            var endpoint = new IPEndPoint(IPAddress.Loopback, 8888);
+            UdpClient socket = new UdpClient(endpoint);
+
+            //Accettazione della connessione(solo per TCP)
+            Console.WriteLine("Server is running");
+            while (true)
+            {
+                try
+                {
+                    //Ricezione e Invio dei dati
+                    var receiveResult = await socket.ReceiveAsync();
+                    byte[] clientMessageBytes = receiveResult.Buffer;
+                    string clientMessage = System.Text.Encoding.ASCII.GetString(clientMessageBytes).TrimEnd('\0');
+
+                    byte[] messageBytes = System.Text.Encoding.ASCII.GetBytes("Echo di " + clientMessage);
+                    await socket.SendAsync(messageBytes, messageBytes.Length);
+                }
+                catch (Exception e)
+                {
+                    break;
+                }
+            }
+            //socket.Dispose();
+        }
+    }
+}
