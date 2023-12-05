8a9da96464750cd1a4c9366202bf5f4d53964429
Davide Luppoli <davide@davideluppoli.it>
2023-12-05 11:16:59 +0100

    Add project files.

---
+++ ClientSocket/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSocket
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Premi un tasto per iniare...");
+            Console.ReadLine();
+
+            //Creare Socket (ip + porta)
+            var endpoint = new IPEndPoint(IPAddress.Loopback, 8888);
+            Socket socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            //Collegarsi al server (ip + porta)
+            await socket.ConnectAsync(endpoint);
+
+            //Generazione Stream di comunicazione
+            NetworkStream networkStream = new NetworkStream(socket,true);
+
+            //Invio dei dati
+            Console.WriteLine("Inserisci il messaggio da inviare:");
+            string message = Console.ReadLine();
+            byte[] messageBytes = System.Text.Encoding.ASCII.GetBytes(message);
+            await networkStream.WriteAsync(messageBytes, 0, messageBytes.Length);
+
+            //Ricezione della risposta
+            byte[] responseBytes = new byte[10000];
+            await networkStream.ReadAsync(responseBytes, 0, responseBytes.Length);
+            string response = System.Text.Encoding.ASCII.GetString(responseBytes).TrimEnd('\0');
+            Console.WriteLine(response);
+            Console.ReadLine();
+
+            //Chiusura Socket
+            networkStream.Close();
+            socket.Dispose();
+        }
+    }
+}
---
+++ ServerSocket/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSocket
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Creazione della socket
+            var endpoint = new IPEndPoint(IPAddress.Loopback, 8888);
+            Socket socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            //Binding della socket(ip address e porta)
+            socket.Bind(endpoint);
+            socket.Listen(128);
+
+            //Accettazione della connessione(solo per TCP)
+            Console.WriteLine("Server is running");
+            while (true)
+            {
+                using (Socket clientSocket = await socket.AcceptAsync())
+                {
+
+                    //Generazione dello stream di comunicazione(solo per TCP)
+                    using (NetworkStream networkStream = new NetworkStream(clientSocket))
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                //Ricezione e Invio dei dati
+                                byte[] clientMessageBytes = new byte[10000];
+                                await networkStream.ReadAsync(clientMessageBytes, 0, clientMessageBytes.Length);
+                                string clientMessage = System.Text.Encoding.ASCII.GetString(clientMessageBytes).TrimEnd('\0');
+
+                                byte[] messageBytes = System.Text.Encoding.ASCII.GetBytes("Echo di " + clientMessage);
+                                await networkStream.WriteAsync(messageBytes, 0, messageBytes.Length);
+                            }
+                            catch (Exception e)
+                            {
+                                break;
+                            }
+                        }
+                        //Chiusura socket
+                        //networkStream.Close();
+                        //clientSocket.Dispose();
+                    }
+                }
+            }
+            //socket.Dispose();
+        }
+    }
+}
