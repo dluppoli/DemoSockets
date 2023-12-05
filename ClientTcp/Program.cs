2bcbd912f0de378388852b003a84a10c963dae0f
Davide Luppoli <davide@davideluppoli.it>
2023-12-05 11:33:57 +0100

    TcpClient

---
+++ ClientTcp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTcp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Premi un tasto per iniare...");
+            Console.ReadLine();
+
+            //Creare Socket (ip + porta)
+            //var endpoint = new IPEndPoint(IPAddress.Loopback, 8888);
+            TcpClient socket = new TcpClient();
+
+            //Collegarsi al server (ip + porta)
+            await socket.ConnectAsync(IPAddress.Loopback, 8888);
+
+            //Generazione Stream di comunicazione
+            NetworkStream networkStream = socket.GetStream();
+
+            //Invio dei dati
+            Console.WriteLine("Inserisci il messaggio da inviare:");
+            string message = Console.ReadLine();
+            byte[] messageBytes = System.Text.Encoding.ASCII.GetBytes(message);
+            await networkStream.WriteAsync(messageBytes, 0, messageBytes.Length);
+
+            //Ricezione della risposta
+            byte[] responseBytes = new byte[10000];
+            await networkStream.ReadAsync(responseBytes, 0, responseBytes.Length);
+            string response = System.Text.Encoding.ASCII.GetString(responseBytes).TrimEnd('\0');
+            Console.WriteLine(response);
+            Console.ReadLine();
+
+            //Chiusura Socket
+            networkStream.Close();
+            socket.Dispose();
+        }
+    }
+}
---
+++ ServerTcp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTcp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Creazione della socket
+            var endpoint = new IPEndPoint(IPAddress.Loopback, 8888);
+            TcpListener socket = new TcpListener(endpoint);
+            socket.Start();
+            //Binding della socket(ip address e porta)
+            //socket.Bind(endpoint);
+            //socket.Listen(128);
+
+            //Accettazione della connessione(solo per TCP)
+            Console.WriteLine("Server is running");
+            while (true)
+            {
+                using (TcpClient clientSocket = await socket.AcceptTcpClientAsync())
+                {
+
+                    //Generazione dello stream di comunicazione(solo per TCP)
+                    using (NetworkStream networkStream = clientSocket.GetStream())
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                //Ricezione e Invio dei dati
+                                byte[] clientMessageBytes = new byte[10000];
+                                await networkStream.ReadAsync(clientMessageBytes, 0, clientMessageBytes.Length);
+                                string clientMessage = System.Text.Encoding.ASCII.GetString(clientMessageBytes).TrimEnd('\0');
+
+                                byte[] messageBytes = System.Text.Encoding.ASCII.GetBytes("Echo di " + clientMessage);
+                                await networkStream.WriteAsync(messageBytes, 0, messageBytes.Length);
+                            }
+                            catch (Exception e)
+                            {
+                                break;
+                            }
+                        }
+                        //Chiusura socket
+                        //networkStream.Close();
+                        //clientSocket.Dispose();
+                    }
+                }
+            }
+            //socket.Dispose();
+        }
+    }
+}
